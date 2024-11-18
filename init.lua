16f7dfcf232067952adbff70667d2b8874f87d2a
PaoloWalsh <paolowalsh7@gmail.com>
2024-11-18 09:53:36 +0100

    catppuccin theme

--- init.lua
+++ init.lua
@@ -623,7 +623,8 @@ require('lazy').setup({
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         clangd = {},
-        phpactor = {},
+        -- phpactor = {},
+        -- cssls = {},
         -- gopls = {},
         pyright = {},
         -- rust_analyzer = {},
@@ -719,8 +720,10 @@ require('lazy').setup({
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
         python = { 'isort', 'black' },
-        cpp = { 'clang-format' },
-        c = { 'clang-format' },
+        -- cpp = { 'clang-format' },
+        -- c = { 'clang-format' },
+        css = { 'prettier' },
+        sql = { 'sql-formatter' },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         javascript = { 'prettierd', 'prettier', stop_after_first = true },
@@ -849,13 +852,15 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
+    'catppuccin/nvim',
+    name = 'catppuccin',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'catppuccin'
+      --'tokyonight-night'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
